e44f3d4b787be3611d11e7128542140c961bedae
ruan carlos <ruaancarlos2602@gmail.com>
2024-04-27 16:13:09 -0300

    Aprendendo um pouco sobre gerenciar senhas e criação de um pequeno projetinho apenas para estudo

---
+++ gerenciapass.py
@@ -0,0 +1,81 @@
+import sqlite3
+
+MASTER_PASSWORD = "123456"
+
+senha = input("Insira a senha do master: ")
+if senha != MASTER_PASSWORD:
+    print("SENHA INVALIDA! ENCERRANDO ...")
+    exit()
+conn = sqlite3.connect("passwords.db")
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        service TEXT NOT NULL,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+''')
+
+def menu():
+    print("************")
+    print("* I : INSERIR NOVA SENHA * ")
+    print("* L : LISTAS SERVIÇOS SALVOS * ")
+    print("* R : RECUPERAR UMA SENHA    * ")
+    print("* S : SAIR                   * ")
+    print("*******************************")
+
+def get_password(service):
+    cursor.execute(f'''
+         SELECT username, password FROM users
+         WHERE service = '{service}'
+    ''')
+
+    if cursor.rowcount == 0:
+        print("Serviço não cadastrado (use 'l' para verificar os serviços).")
+
+    else:
+        for user in cursor.fetchall():
+            print(user)
+
+def insert_password(service, username, password):
+        cursor.execute(f'''
+        INSERT INTO users (service, username, password)
+        VALUES ('{service}', '{username}', '{password}')
+       ''')
+        conn.commit()
+
+def show_services():
+        cursor.execute('''
+             SELECT service FROM users;
+        ''')
+        for service in cursor.fetchall():
+            print(service)
+
+while True:
+    menu()
+    op = input("O que deseja fazer? ")
+    if op not in ["l", "i", "r", "s"]:
+        print(" Opção invalida!" )
+        continue
+
+    if op == "s":
+        break
+
+    if op == "i":
+        service = input("Qual o nome do serviço? ")
+        username = input("Qual o nome de usuario? ")
+        password = input("Qual a senha? ")
+        insert_password(service, username, password)
+
+
+    if op == "l":
+        show_services()
+
+    if op == "r":
+        service = input("Qual o serviço para o qual quer a senha? ")
+        get_password(service)
+
+
+
+conn.close()
